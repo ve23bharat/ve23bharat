1ae74c01a195db686dae41a1709b5a927021ae41
Bharat Kumar Verma <ve23bharat@gmail.com>
2022-12-29 08:17:12 +0530

    This is my 2nd commit trying to learn

---
+++ f1.py
@@ -0,0 +1,3 @@
+
+Today I am trying to do git push and fetch
+so therefore I am trying to make local repository file into global repository file.
---
+++ f2.py
@@ -0,0 +1,2 @@
+
+When file used to create via touch then just file is created.
